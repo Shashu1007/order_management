8dd1bf16f83acc53d2e53fec0ea78837fa1682e1
Shashank R <sshashu777@gmail.com>
2024-12-05 17:41:46 +0530

    modified

--- src/main/java/com/orderiFy/app/config/JwtFilter.java
+++ src/main/java/com/orderiFy/app/config/JwtFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
